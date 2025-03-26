52e6b16ec6824796f23a136f6c080d9cae4dcace
Louisa-May <84612674+Louisa-May@users.noreply.github.com>
2025-03-26 01:22:55 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "demo_subnet" {
   vpc_id     = aws_vpc.demo.id
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
+  availability_zone = "eu-west-1"
 
   tags = {
     Name = "demo_subnet"
@@ -103,8 +103,8 @@ resource "aws_instance" "foo" {
   instance_type = "t2.micro"
   subnet_id = aws_subnet.demo_subnet.id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  availability_zone = "us-east-1a"
-  key_name = "devopskeypair"
+  availability_zone = "eu-west-1"
+  key_name = "ajibskeypair"
   count = 5
 
   tags = {
